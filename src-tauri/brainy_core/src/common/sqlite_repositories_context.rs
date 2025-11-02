3b061ecbf32ea010126efa9a88f84f287ff5ff11
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-11-02 10:01:29 +0100

    fix(sync): use shared cache for database
    The reading connection is different from the transaction one used in
    repositories. This makes some errors for syncing, e.g. adding a file to
    a folder deleted from other device.

--- src-tauri/brainy_core/src/common/sqlite_repositories_context.rs
+++ src-tauri/brainy_core/src/common/sqlite_repositories_context.rs
@@ -56,7 +56,8 @@ impl SqliteRepositoriesContext {
     pub async fn new_with_migration(path: &str) -> Result<Self, SqliteRepositoriesContextError> {
         let url = format!("{path}?mode=rwc");
         let options = SqliteConnectOptions::from_str(&url)?
-            // Since there is a single client, we can allow read uncommitted.
+            // Since there is a single client, we can allow read uncommitted, and use shared cache.
+            .shared_cache(true)
             .pragma("read_uncommitted", "TRUE")
             .optimize_on_close(true, None);
         let pool = SqlitePoolOptions::new().connect_with(options).await?;
