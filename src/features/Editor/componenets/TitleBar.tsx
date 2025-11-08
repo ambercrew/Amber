d17778bcb4f3701f789d01d5621ddcd0fcdba8d5
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-11-08 09:47:19 +0100

    feat(editor): make the searchbar expandable on small screens

--- src/features/Editor/componenets/TitleBar.tsx
+++ src/features/Editor/componenets/TitleBar.tsx
@@ -8,9 +8,9 @@ import { useSearchParams } from "react-router";
 import { fileIdQueryParameter } from "../../../config/constants";
 import InputWithIcon from "../../../components/InputWithIcon/InputWithIcon";
 import useGlobalKey from "../../../hooks/useGlobalKey";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-interface Props {
+interface IProps {
 	repetitionCounts: FileRepetitionCounts;
 	searchText: string;
 	onSearchTextChange: (value: string) => void;
@@ -26,7 +26,9 @@ function TitleBar({
 	onStudyButtonClick,
 	onSearchInputFocus,
 	onSearchInputBlur,
-}: Props) {
+}: IProps) {
+	// Only used for small screens.
+	const [showSearchBar, setShowSearchBar] = useState(false);
 	const [searchParams] = useSearchParams();
 	const searchInputRef = useRef<HTMLInputElement>(null);
 	const selectedFileId = searchParams.get(fileIdQueryParameter);
@@ -44,34 +46,63 @@ function TitleBar({
 	useGlobalKey(e => {
 		if (e.ctrlKey && !e.shiftKey && e.key.toLowerCase() === "f") {
 			e.preventDefault();
+			setShowSearchBar(true);
 			searchInputRef.current?.focus();
 		}
 	}, "keydown");
 
+	useEffect(() => {
+		if (!searchInputRef.current) return;
+
+		if (showSearchBar) searchInputRef.current.focus();
+		// Removing search text when the search bar is hidden.
+		else onSearchTextChange("");
+	}, [showSearchBar, onSearchTextChange]);
+
+	const handleSearchInputFocus = () => {
+		setShowSearchBar(true);
+		onSearchInputFocus();
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Escape") {
+			setShowSearchBar(false);
+		}
+	};
+
 	return (
 		<div className={styles.titleBar}>
-			<div className={styles.info}>
-				<button
-					className={`transparent ${styles.studyButton}`}
-					onClick={onStudyButtonClick}
-					disabled={isStudyButtonDisabled}>
-					<Icon path={mdiPlayOutline} size={1} />
-					<span className={styles.label}>Study</span>
-				</button>
-				<div>
-					<p>{selectedFile?.name}</p>
-					<div className={styles.repetitionCounts}>
-						<span>New: {repetitionCounts.new}</span>
-						<span>&#x2022;</span>
-						<span>
-							Learn:&nbsp;
-							{repetitionCounts.learning +
-								repetitionCounts.relearning}
-						</span>
-						<span>&#x2022;</span>
-						<span>Review: {repetitionCounts.review}</span>
+			<div className={styles.titleAndSearchButton}>
+				<div className={styles.info}>
+					<button
+						className={`transparent ${styles.studyButton}`}
+						onClick={onStudyButtonClick}
+						disabled={isStudyButtonDisabled}>
+						<Icon path={mdiPlayOutline} size={1} />
+						<span className={styles.label}>Study</span>
+					</button>
+					<div>
+						<p>{selectedFile?.name}</p>
+						<div className={styles.repetitionCounts}>
+							<span>New: {repetitionCounts.new}</span>
+							<span>&#x2022;</span>
+							<span>
+								Learn:&nbsp;
+								{repetitionCounts.learning +
+									repetitionCounts.relearning}
+							</span>
+							<span>&#x2022;</span>
+							<span>Review: {repetitionCounts.review}</span>
+						</div>
 					</div>
 				</div>
+
+				{/* Only shown on small screens */}
+				<button
+					className={`transparent ${styles.searchButton}`}
+					onClick={() => setShowSearchBar(!showSearchBar)}>
+					<Icon path={mdiMagnify} size={1} />
+				</button>
 			</div>
 
 			<InputWithIcon
@@ -79,11 +110,12 @@ function TitleBar({
 				placeholder="Search (Ctrl + F)"
 				value={searchText}
 				onChange={e => onSearchTextChange(e.target.value)}
-				containerClassName={styles.searchInputContainer}
+				containerClassName={`${styles.searchInputContainer} ${showSearchBar && styles.shown}`}
 				ref={searchInputRef}
 				className={styles.searchInput}
-				onFocus={onSearchInputFocus}
+				onFocus={handleSearchInputFocus}
 				onBlur={onSearchInputBlur}
+				onKeyDown={handleKeyDown}
 			/>
 		</div>
 	);
