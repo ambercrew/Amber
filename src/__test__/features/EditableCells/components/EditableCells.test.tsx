f6d67c626d9e247519acc1b340690e7989d8d466
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-03 21:58:39 +0100

    test(editableCells): add tests for scrolling and logic

--- src/__test__/features/EditableCells/components/EditableCells.test.tsx
+++ src/__test__/features/EditableCells/components/EditableCells.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { act, fireEvent, screen, waitFor } from "@testing-library/react";
 import EditableCells from "../../../../features/EditableCells/components/EditableCells";
 import createDefaultCell from "../../../../features/EditableCells/utils/createDefaultCell";
 import Cell from "../../../../types/backend/entity/cell";
@@ -296,5 +296,109 @@ describe("EditableCells scrolling", () => {
 
 		expect(cellsScrolledTo).toContain("CellBlock-3");
 		expect(cellsScrolledTo).toHaveLength(1);
+	});
+
+	it("Should scroll when deleting Cell", async () => {
+		// Arrange
+
+		setBoundingClientRectByTestId({
+			"CellBlock-2": {
+				bottom: 30,
+			},
+			EditableCells: {
+				bottom: 20,
+			},
+		});
+
+		const cells = [createTestCell(1), createTestCell(2), createTestCell(3)];
+		renderEditableCells({
+			cells,
+			initialSelectedCellId: "3",
+			onCellsUpdateSave: () => [createTestCell(1), createTestCell(2)],
+		});
+
+		// Act
+
+		await userEvent.click(screen.getByTitle("Delete cell (Alt + Del)"));
+		await userEvent.click(screen.getByText("Yes"));
+
+		// Assert
+
+		expect(cellsScrolledTo).toContain("CellBlock-2");
+		expect(cellsScrolledTo).toHaveLength(1);
+	});
+
+	it("Should scroll when dropping", async () => {
+		// Arrange
+
+		setBoundingClientRectByTestId({
+			"CellBlock-3": {
+				bottom: 30,
+			},
+			EditableCells: {
+				bottom: 20,
+			},
+		});
+
+		const cells = [createTestCell(1), createTestCell(2), createTestCell(3)];
+		renderEditableCells({
+			cells,
+			initialSelectedCellId: "3",
+			onCellsUpdateSave: () => [
+				createTestCell(1),
+				createTestCell(3),
+				createTestCell(2),
+			],
+		});
+
+		// Act
+
+		act(() => {
+			fireEvent.drop(screen.getByTestId("CellBlock-2"), {
+				dataTransfer: {
+					getData() {
+						return "3";
+					},
+				},
+			});
+		});
+
+		// Assert
+
+		await waitFor(() => {
+			expect(cellsScrolledTo).toContain("CellBlock-3");
+			expect(cellsScrolledTo).toHaveLength(2);
+		});
+	});
+});
+
+describe("EditableCells logic", () => {
+	beforeAll(() => {
+		// @ts-expect-error IntersectionObserver is not found by default on the testing library.
+		window.IntersectionObserver = class IntersectionObserver {
+			constructor(cb: (entries: unknown[]) => void) {
+				cb([
+					{
+						isIntersecting: true,
+					},
+				]);
+			}
+
+			observe = vi.fn();
+			unobserve = vi.fn();
+		};
+	});
+
+	it("Should always select a cell at the start", () => {
+		// Act
+
+		renderEditableCells({
+			cells: [createTestCell(1)],
+		});
+
+		// Assert
+
+		const classList = screen.getByTestId("CellBlock-1").classList.value;
+		expect(classList).contains("selected-cell");
 	});
 });
