b39d6aa9be3401ffa880dd6e095c1dfc7749602d
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-06 18:13:24 +0100

    test(App): add unit test for navigation

--- src/__test__/app/App.test.tsx
+++ src/__test__/app/App.test.tsx
@@ -14,7 +14,13 @@ import {
 import userEvent from "@testing-library/user-event";
 import { check, Update } from "@tauri-apps/plugin-updater";
 import { ask } from "@tauri-apps/plugin-dialog";
-import sideBarStyles from "../../features/SideBar/componenets/styles.module.css";
+import appStyles from "../../app/styles.module.css";
+import sideBarStyles from "../../features/SideBar/components/styles.module.css";
+import homeStyles from "../../features/Home/components/styles.module.css";
+import editorStyles from "../../features/Editor/components/styles.module.css";
+import reviewerStyles from "../../features/Reviewer/components/styles.module.css";
+import searcherStyles from "../../features/Searcher/components/styles.module.css";
+import { SMALL_SCREEN_MAX_WIDTH_IN_PX } from "../../config/constants";
 
 vi.mock(import("../../hooks/useAppDispatch"), () => ({
 	default: vi.fn(),
@@ -22,6 +28,7 @@ vi.mock(import("../../hooks/useAppDispatch"), () => ({
 vi.mock(import("../../stores/fileSystem/fileSystemActions"));
 vi.mock(import("../../stores/user/userActions"));
 vi.mock(import("../../stores/settings/settingsActions"));
+vi.mock(import("../../managers/closeRequestedEventManager"));
 vi.mock(import("@tauri-apps/api/core"));
 vi.mock(import("@tauri-apps/plugin-updater"));
 vi.mock(import("@tauri-apps/plugin-dialog"));
@@ -184,7 +191,7 @@ describe("App", () => {
 	it("Should navigate to home on shortcut", async () => {
 		// Arrange
 
-		const { history } = renderWithProviders(<App />);
+		renderWithProviders(<App />);
 
 		// Act
 
@@ -192,7 +199,9 @@ describe("App", () => {
 
 		// Assert
 
-		expect(history.location.pathname).toBe("/home");
+		expect(screen.getByTestId("location-display")).toHaveTextContent(
+			"/home",
+		);
 	});
 
 	it("Should render updater", async () => {
@@ -238,5 +247,136 @@ describe("App", () => {
 
 		const sideBar = screen.getByRole("complementary");
 		expect(sideBar.className).toContain(sideBarStyles.closed);
+	});
+
+	it("Should render home when on /home", () => {
+		// Act
+
+		const { container } = renderWithProviders(<App />, {
+			memoryRouterProps: {
+				initialEntries: ["/home"],
+			},
+		});
+
+		// Assert
+
+		expect(container.getElementsByClassName(homeStyles.home).length).toBe(
+			1,
+		);
+	});
+
+	it("Should render editor when on /editor", () => {
+		// Act
+
+		const { container } = renderWithProviders(<App />, {
+			memoryRouterProps: {
+				initialEntries: ["/editor"],
+			},
+		});
+
+		// Assert
+
+		expect(
+			container.getElementsByClassName(editorStyles.container).length,
+		).toBe(1);
+	});
+
+	it("Should render reviewer when on /reviewer", () => {
+		// Act
+
+		const { container } = renderWithProviders(<App />, {
+			memoryRouterProps: {
+				initialEntries: ["/reviewer"],
+			},
+		});
+
+		// Assert
+
+		expect(
+			container.getElementsByClassName(reviewerStyles.reviewer).length,
+		).toBe(1);
+	});
+
+	it("Should render searcher when on /search", () => {
+		// Act
+
+		const { container } = renderWithProviders(<App />, {
+			memoryRouterProps: {
+				initialEntries: ["/search"],
+			},
+		});
+
+		// Assert
+
+		expect(
+			container.getElementsByClassName(searcherStyles.container).length,
+		).toBe(1);
+	});
+
+	it("Should add hidden class on work-area when screen is small and sidebar is expanded", () => {
+		// Arrange
+
+		window.innerWidth = SMALL_SCREEN_MAX_WIDTH_IN_PX;
+
+		// Act
+
+		const { container } = renderWithProviders(<App />);
+
+		// Assert
+
+		expect(container.getElementsByClassName(appStyles.hidden).length).toBe(
+			1,
+		);
+	});
+
+	it("Should not add hidden class on work-area when screen is not small and sidebar is expanded", () => {
+		// Arrange
+
+		window.innerWidth = SMALL_SCREEN_MAX_WIDTH_IN_PX + 1;
+
+		// Act
+
+		const { container } = renderWithProviders(<App />);
+
+		// Assert
+
+		expect(container.getElementsByClassName(appStyles.hidden).length).toBe(
+			0,
+		);
+	});
+
+	it("Should not add hidden class on work-area when screen is small and sidebar is not expanded", async () => {
+		// Arrange
+
+		window.innerWidth = SMALL_SCREEN_MAX_WIDTH_IN_PX;
+		const { container } = renderWithProviders(<App />);
+
+		// Act
+
+		await userEvent.keyboard("{Control>}\\");
+
+		// Assert
+
+		expect(container.getElementsByClassName(appStyles.hidden).length).toBe(
+			0,
+		);
+	});
+
+	it("Should collapse sidebar when navigating on small screen", async () => {
+		// Arrange
+
+		window.innerWidth = SMALL_SCREEN_MAX_WIDTH_IN_PX;
+		renderWithProviders(<App />);
+
+		// Act
+
+		await userEvent.click(screen.getByText("Search"));
+
+		// Assert
+
+		await waitFor(() => {
+			const sideBar = screen.getByRole("complementary");
+			expect(sideBar.className).toContain(sideBarStyles.closed);
+		});
 	});
 });
