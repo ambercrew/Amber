a0f371905246bbb388c21950079f46abc266beb5
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-06 19:21:21 +0100

    test(cancellableInput): add unit tests

---
+++ src/__test__/componenets/CancellableInput.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import CancellableInput from "../../components/CancellableInput/CancellableInput";
+import userEvent from "@testing-library/user-event";
+
+describe("CancellableInput", () => {
+	it("Should calls onCancel when pressing Escape", async () => {
+		// Arrange
+
+		const fn = vi.fn();
+		render(
+			<CancellableInput onCancel={fn} placeholder={"CancellableInput"} />,
+		);
+
+		// Act
+
+		// Focusing on the input.
+		await userEvent.click(screen.getByPlaceholderText("CancellableInput"));
+		await userEvent.keyboard("{Escape}");
+
+		// Assert
+
+		expect(fn).toBeCalled();
+	});
+});
