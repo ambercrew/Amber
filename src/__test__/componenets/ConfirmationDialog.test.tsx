58e8def5db823cff338689590f7fd8c3ba811e47
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-06 19:21:28 +0100

    test(confirmationDialog): add unit tests

---
+++ src/__test__/componenets/ConfirmationDialog.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ConfirmationDialog from "../../components/ConfirmationDialog/ConfirmationDialog";
+
+describe("ConfirmationDialog", () => {
+	it("Should calls onCancel when clicking on No", async () => {
+		// Arrange
+
+		const onCancel = vi.fn();
+		render(
+			<ConfirmationDialog
+				onCancel={onCancel}
+				onConfirm={vi.fn()}
+				title=""
+				text=""
+			/>,
+		);
+
+		// Act
+
+		// Focusing on the input.
+		await userEvent.click(screen.getByText("No"));
+
+		// Assert
+
+		expect(onCancel).toBeCalled();
+	});
+
+	it("Should calls onConfirm when clicking on Yes", async () => {
+		// Arrange
+
+		const onConfirm = vi.fn();
+		render(
+			<ConfirmationDialog
+				onCancel={vi.fn()}
+				onConfirm={onConfirm}
+				title=""
+				text=""
+			/>,
+		);
+
+		// Act
+
+		// Focusing on the input.
+		await userEvent.click(screen.getByText("Yes"));
+
+		// Assert
+
+		expect(onConfirm).toBeCalled();
+	});
+});
