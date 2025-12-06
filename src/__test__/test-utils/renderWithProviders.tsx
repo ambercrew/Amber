2822e42c128082a853650dbd03246c596b6e7d19
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-06 11:59:15 +0100

    test(renderWithProviders): add React Router integration

--- src/__test__/test-utils/renderWithProviders.tsx
+++ src/__test__/test-utils/renderWithProviders.tsx
@@ -3,22 +3,36 @@ import { render } from "@testing-library/react";
 import type { RenderOptions } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { AppStore, RootState, setupStore } from "../../stores/store";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router";
 
-interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+interface IExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
 	preloadedState?: Partial<RootState>;
 	store?: AppStore;
 }
 
+/** A helper function for rendering that sets the default store used in the app,
+ * additionally it adds a React Router.
+ */
 export function renderWithProviders(
 	ui: React.ReactElement,
 	{
 		preloadedState = {},
 		store = setupStore(preloadedState),
 		...renderOptions
-	}: ExtendedRenderOptions = {},
+	}: IExtendedRenderOptions = {},
 ) {
+	const history = createMemoryHistory();
 	function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
-		return <Provider store={store}>{children}</Provider>;
+		return (
+			<Router location={history.location} navigator={history}>
+				<Provider store={store}>{children}</Provider>
+			</Router>
+		);
 	}
-	return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+	return {
+		store,
+		history,
+		...render(ui, { wrapper: Wrapper, ...renderOptions }),
+	};
 }
