7e79ec85fcdc72db651143e236769254e4057584
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-07 16:50:56 +0100

    test(useGlobalKey): add tests

---
+++ src/__test__/hooks/useGlobalKey.test.ts
@@ -0,0 +1,40 @@
+import { act, fireEvent, renderHook, waitFor } from "@testing-library/react";
+import useGlobalKey from "../../hooks/useGlobalKey";
+import userEvent from "@testing-library/user-event";
+
+describe("useGlobalKey", () => {
+	it("Should call the callback function", async () => {
+		// Arrange
+
+		const cb = vi.fn();
+		renderHook(() => useGlobalKey(cb));
+
+		// Act
+
+		await userEvent.keyboard("t");
+
+		// Assert
+
+		await waitFor(() => {
+			expect(cb).toBeCalled();
+		});
+	});
+
+	it("Should unregister the event on unmount", async () => {
+		// Arrange
+
+		const cb = vi.fn();
+		const { unmount } = renderHook(() => useGlobalKey(cb));
+		unmount();
+
+		// Act
+
+		await userEvent.keyboard("t");
+
+		// Assert
+
+		await waitFor(() => {
+			expect(cb).not.toBeCalled();
+		});
+	});
+});
