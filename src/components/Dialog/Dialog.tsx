fcd5c61c2a58cde002470eeb7293ecf76f05b168
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-12-07 11:32:32 +0100

    chore(dialog): fix spellings

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 
-interface Props {
+interface IProps {
 	className?: string;
 	children: React.ReactNode;
 	onHide?: () => void;
@@ -10,11 +10,11 @@ interface Props {
 /** Shows a dialog with an overlay that handles the hide logic such as hiding
  * on overlay click, pressing Escape. Additionally it moves the focus to the
  * element that had the focus before the dialog was shown.
- * If the children of the dialog is a form, and the dialog is not immeditaly
+ * If the children of the dialog is a form, and the dialog is not immediately
  * hidden after submit, stop the submit event propagation, otherwise the dialog,
  * might move the focus away!
  */
-export default function Dialog({ className, children, onHide }: Props) {
+export default function Dialog({ className, children, onHide }: IProps) {
 	const [focusedElementBeforeView, setFocusedElementBeforeView] =
 		useState<HTMLElement | null>(null);
 
