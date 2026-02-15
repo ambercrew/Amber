c66bfc204420b889e387308a04ed42d2a9b96994
Rami Alkawadri <ramialkawadri@hotmail.com>
2026-02-15 19:57:23 +0100

    fix: remove italic font

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import "@fontsource-variable/noto-sans/wdth-italic.css";
+import "@fontsource-variable/noto-sans";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
