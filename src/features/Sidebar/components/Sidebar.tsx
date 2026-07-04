9a91feda115e1498b2aadb4fb3de9012c9feb59c
Rami Alkawadri <ramialkawadri@hotmail.com>
2026-07-04 08:27:51 +0200

    Use scrollarea

--- src/features/Sidebar/components/Sidebar.tsx
+++ src/features/Sidebar/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Tabs, Group, ActionIcon } from "@mantine/core";
+import { AppShell, Tabs, Group, ActionIcon, ScrollArea } from "@mantine/core";
 import ElementTreePanel from "./ElementTreePanel";
 import PriorityQueuePanel from "./PriorityQueuePanel";
 import { TreeViewIcon, QueueIcon, XIcon } from "@phosphor-icons/react";
@@ -9,38 +9,40 @@ interface SidebarProps {
 
 function Sidebar({ onCollapse }: SidebarProps) {
 	return (
-		<AppShell.Section py="sm" grow style={{ overflowY: "auto" }}>
-			<Tabs defaultValue="tree" variant="pills">
-				<Group justify="center" style={{ position: "relative" }}>
-					<Tabs.List>
-						<Tabs.Tab
-							value="tree"
-							title="Element Tree - used for managing your learning materials">
-							<TreeViewIcon size={16} />
-						</Tabs.Tab>
-						<Tabs.Tab
-							value="priority-queue"
-							title="Priority queue - used for reviewing your learning materials">
-							<QueueIcon size={16} />
-						</Tabs.Tab>
-					</Tabs.List>
-					<ActionIcon
-						variant="subtle"
-						onClick={onCollapse}
-						hiddenFrom="sm"
-						mx="md"
-						style={{ position: "absolute", right: 0 }}>
-						<XIcon size={18} />
-					</ActionIcon>
-				</Group>
+		<AppShell.Section py="sm" grow h="100%">
+			<ScrollArea h="100%">
+				<Tabs defaultValue="tree" variant="pills">
+					<Group justify="center" style={{ position: "relative" }}>
+						<Tabs.List>
+							<Tabs.Tab
+								value="tree"
+								title="Element Tree - used for managing your learning materials">
+								<TreeViewIcon size={16} />
+							</Tabs.Tab>
+							<Tabs.Tab
+								value="priority-queue"
+								title="Priority queue - used for reviewing your learning materials">
+								<QueueIcon size={16} />
+							</Tabs.Tab>
+						</Tabs.List>
+						<ActionIcon
+							variant="subtle"
+							onClick={onCollapse}
+							hiddenFrom="sm"
+							mx="md"
+							style={{ position: "absolute", right: 0 }}>
+							<XIcon size={18} />
+						</ActionIcon>
+					</Group>
 
-				<Tabs.Panel value="tree">
-					<ElementTreePanel />
-				</Tabs.Panel>
-				<Tabs.Panel value="priority-queue">
-					<PriorityQueuePanel />
-				</Tabs.Panel>
-			</Tabs>
+					<Tabs.Panel value="tree">
+						<ElementTreePanel />
+					</Tabs.Panel>
+					<Tabs.Panel value="priority-queue">
+						<PriorityQueuePanel />
+					</Tabs.Panel>
+				</Tabs>
+			</ScrollArea>
 		</AppShell.Section>
 	);
 }
